ac3b19393428b150ede9585696a33506abba29ef
Adam Hani Schakaki <adam@schakaki.net>
2013-03-22 11:08:06 +0000

    Remove superfluous settings

--- server.py
+++ server.py
@@ -11,10 +11,6 @@ def error(status, message, traceback, version):
     return json.dumps({'status': status, 'message': message})
 
 if __name__ == '__main__':
-    config = {'global': {'server.socket_host': '0.0.0.0',
-                         'server.socket_port': 80,
-                        },
-              '/':      {'error_page.default': error}
-             }
+    config = {'/': {'error_page.default': error}}
     app = cherrypy.tree.mount(controller.ShnergleServer(), "/", config)
     wsgiref.handlers.CGIHandler().run(app)

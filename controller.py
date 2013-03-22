f8be4f9c41dc16b6b2be1970ecaded7b617b950c
Adam Hani Schakaki <adam@schakaki.net>
2013-03-22 14:50:50 +0000

    Follow PEP 8

--- controller.py
+++ controller.py
@@ -8,11 +8,17 @@ from functools import wraps
 def jsonp(func):
     @wraps(func)
     def decorator(*args, **kwargs):
-        cherrypy.response.headers['Expires'] = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-        cherrypy.response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+        cherrypy.response.headers['Expires'] = datetime.utcnow().strftime(
+            '%a, %d %b %Y %H:%M:%S GMT')
+        cherrypy.response.headers['Cache-Control'] = ('no-store, '
+                                                      'no-cache, '
+                                                      'must-revalidate, '
+                                                      'post-check=0, '
+                                                      'pre-check=0')
         res = json.dumps(func(*args, **kwargs))
         if 'callback' in kwargs:
-            cherrypy.response.headers['Content-Type'] = 'text/javascript; charset=utf-8'
+            cherrypy.response.headers['Content-Type'] = ('text/javascript; '
+                                                         'charset=utf-8')
             res = kwargs['callback'] + '(' + res + ');'
         else:
             cherrypy.response.headers['Content-Type'] = 'application/json'

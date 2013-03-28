f5168835ce47a65252f1cc444e3296a791ac9cac
Adam Hani Schakaki <adam@schakaki.net>
2013-03-28 14:12:57 +0000

    Parameter handling is unneccessary

--- mongorest.py
+++ mongorest.py
@@ -8,14 +8,9 @@ class MongoRest:
     def __init__(self, db='shnergle'):
         self.db = db
         
-    def url(self, collection='', id='', args={}):
+    def url(self, collection='', id=''):
         if collection:
             collection = '/' + collection
         if id:
             id = '/' + str(id)
-        args[self._apikeyp] = self._apikey
-        args_str = '&'.join(
-            [key + '=' + value for key, value in args.iteritems()])
-        if args_str:
-            args_str = '?' + args_str
-        return self._url + self.db + '/collections' + collection + id + args_str
+        return self._url + self.db + '/collections' + collection + id

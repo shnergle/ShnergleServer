f92f26092c074b20c13ac3b5d97f0c2f65d8a4fc
Adam Hani Schakaki <adam@schakaki.net>
2013-03-28 12:53:41 +0000

    URL creator for mongo rest api

--- mongorest.py
+++ mongorest.py
@@ -1,4 +1,21 @@
 import requests
 
 class MongoRest:
-    pass
+    _apikeyp = 'apiKey'
+    _apikey = 'O7VCBY3vn25Qpa3byNmRFN920L7KOPIj'
+    _url = 'https://api.mongolab.com/api/1/databases/'
+    
+    def __init__(self, db='shnergle'):
+        self.db = db
+        
+    def url(self, collection='', id='', args={}):
+        if collection:
+            collection = '/' + collection
+        if id:
+            id = '/' + str(id)
+        args[self._apikeyp] = self._apikey
+        args_list = []
+        for key, value in args.iteritems():
+            args_list.append(key + '=' + value)
+        args_str = '?' + '&'.join(args_list)
+        return self._url + self.db + '/collections' + collection + id + args_str

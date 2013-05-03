dc1c901bf6df64b144245cdeac3e58fc55eeedfd
Adam Hani Schakaki <adam@schakaki.net>
2013-05-03 14:22:31 +0100

    Revert "WSGI Log"
    This reverts commit 102bddf9257e6df29da4845c30d94263afb49696.

--- server.py
+++ server.py
@@ -229,8 +229,7 @@ class ShnergleServer:
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-cp_config = {'/':            {'error_page.default': ShnergleServer.error,
-                              'log.wsgi': True},
+cp_config = {'/':            {'error_page.default': ShnergleServer.error},
              '/favicon.ico': {'tools.staticfile.on': True,
                               'tools.staticfile.filename':
                               os.path.join(current_dir, 'favicon.ico')}}

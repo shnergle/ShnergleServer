8a0a513868a84b4140c60ba4068bad13e0a9db7f
Adam Hani Schakaki <adam@schakaki.net>
2013-05-03 16:28:19 +0100

    MSSQL does not like double quotes

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ class User:
                              'users.employee',
                              'users.joined',
                              'countries.code AS country',
-                             '(LOWER(languages.code) + "_" + '
+                             '(LOWER(languages.code) + \'_\' + '
                                     'UPPER(lc.code)) AS language',
                              'COUNT(posts.id) AS post_count'],
                'table':      'users',

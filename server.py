102bddf9257e6df29da4845c30d94263afb49696
Adam Hani Schakaki <adam@schakaki.net>
2013-05-03 14:21:40 +0100

    WSGI Log

--- server.py
+++ server.py
@@ -229,7 +229,8 @@ class ShnergleServer:
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-cp_config = {'/':            {'error_page.default': ShnergleServer.error},
+cp_config = {'/':            {'error_page.default': ShnergleServer.error,
+                              'log.wsgi': True},
              '/favicon.ico': {'tools.staticfile.on': True,
                               'tools.staticfile.filename':
                               os.path.join(current_dir, 'favicon.ico')}}

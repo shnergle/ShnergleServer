45a628fb3390c3f3228d64714d8c5f39474580d1
Adam Hani Schakaki <adam@schakaki.net>
2013-07-04 14:34:51 +0100

    debug

--- api/main.py
+++ api/main.py
@@ -430,6 +430,7 @@ class VenueFavourite:
         if util.to_bool(following) and not res:
             qry = {'insert_into': 'venue_favourites',
                    'columns':      ('user_id, venue_id')}
+            raise Exception(util.query(**qry))
             cursor.execute(util.query(**qry), (user_id, venue_id))
         elif not util.to_bool(following) and res:
             #delete

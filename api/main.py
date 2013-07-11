c33064a62e07486af1d8a5e353662c73f35e01a0
Adam Hani Schakaki <adam@schakaki.net>
2013-07-11 16:44:37 +0100

    ahem

--- api/main.py
+++ api/main.py
@@ -152,7 +152,7 @@ class PostShare:
             **kwargs):
         qry = {'insert_into': 'post_shares',
                'columns':     ('user_id', 'post_id', 'media_id')}
-        cursor.execute(util.query(**qry), (user_id, venue_id, menu_id))
+        cursor.execute(util.query(**qry), (user_id, post_id, menu_id))
         return True
         
         

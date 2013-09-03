1998fc81957a1d1fb19f6bf95f52b6b3b0457687
Adam Hani Schakaki <adam@schakaki.net>
2013-09-03 19:19:19 +0100

    .

--- api/v2/main.py
+++ api/v2/main.py
@@ -1,11 +1,27 @@
 import cherrypy
+import json
 
-class Image:
+class image:
     
     @staticmethod
     @cherrypy.expose
     def get():
         return 'Have an image!'
+        
+        
+class user:
+    
+    @staticmethod
+    @cherrypy.expose
+    def get():
+        cursor = cherrypy.thread_data.db.cursor()
+        try:
+            cursor.execute('SELECT TOP(1) * FROM users WHERE id = 1')
+            json.dumps(cursor.fetchone())
+        finally:
+            cursor.commit()
+            cursor.close()
+        return res
 
 
 @cherrypy.expose

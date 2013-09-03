fafa282ae57abb757782114836dbdaea33a22c60
Adam Hani Schakaki <adam@schakaki.net>
2013-09-03 19:35:47 +0100

    .

--- api/v2/main.py
+++ api/v2/main.py
@@ -13,11 +13,27 @@ class user:
     
     @staticmethod
     @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
     def get():
         cursor = cherrypy.thread_data.db.cursor()
         try:
             cursor.execute('SELECT TOP(1) * FROM users WHERE id = 1')
-            res = json.dumps({t[0]: val for t, val in zip(cursor.description, cursor.fetchone())})
+            res = {t[0]: val for t, val in zip(cursor.description, cursor.fetchone())}
+        finally:
+            cursor.commit()
+            cursor.close()
+        return res
+       
+    @staticmethod
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def login():
+        cursor = cherrypy.thread_data.db.cursor()
+        try:
+            cursor.execute('SELECT TOP(1) * FROM users WHERE facebook_id = ?', cherrypy.request.json['facebook_id'])
+            res = {t[0]: val for t, val in zip(cursor.description, cursor.fetchone())}
         finally:
             cursor.commit()
             cursor.close()
